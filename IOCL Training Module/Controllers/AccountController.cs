9bc2811bb8ba84d13bd239a8f47ad7f303c22b6e
Samridh Barthaval <samridh2305@gmail.com>
2025-02-28 12:25:13 +0530

    entered option for resetting password

--- IOCL Training Module/Controllers/AccountController.cs
+++ IOCL Training Module/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using IOCL_Training_Module.Data; // Database context
 using IOCL_Training_Module.Models; // Employee model
 using Microsoft.AspNetCore.Http; // For session handling
 using System.Linq; // For LINQ queries
+using System.Threading.Tasks;
 
 public class AccountController : Controller
 {
@@ -25,7 +26,7 @@ public class AccountController : Controller
 
         if (employee != null)
         {
-            if (employee.Password == Password) // Plaintext check (Consider Hashing)
+            if (employee.Password == Password) // Directly compare stored password
             {
                 HttpContext.Session.SetString("EmpNo", EmpNo);
                 return RedirectToAction("Index", "Dashboard", new { empNo = EmpNo });
@@ -39,6 +40,41 @@ public class AccountController : Controller
         {
             ViewBag.Error = "Invalid Employee Number. Please try again.";
         }
+
+        return View();
+    }
+
+    // GET: Forgot Password Page
+    [HttpGet]
+    public IActionResult ForgotPassword()
+    {
+        return View();
+    }
+
+    // POST: Reset Password
+    [HttpPost]
+    public async Task<IActionResult> ForgotPassword(string EmpNo, string NewPassword, string ConfirmPassword)
+    {
+        if (NewPassword != ConfirmPassword)
+        {
+            ViewBag.Message = "Passwords do not match!";
+            return View();
+        }
+
+        var employee = _context.Employees.FirstOrDefault(e => e.EmpNo == EmpNo);
+
+        if (employee == null)
+        {
+            ViewBag.Message = "Employee number not found!";
+            return View();
+        }
+
+        // Update the password directly in the database
+        employee.Password = NewPassword;
+        _context.Employees.Update(employee);
+        await _context.SaveChangesAsync();
+
+        ViewBag.Message = "Password reset successfully! You can now login with your new password.";
 
         return View();
     }
